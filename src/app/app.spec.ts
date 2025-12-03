250ab8d22c5aba0420412ae72ee4e65335af8135
Lucas da Silva Paz <149909352+lucaspaz4486@users.noreply.github.com>
2025-12-03 10:41:07 -0300

    Corrigindo nome da classe no arquivo de teste

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,31 @@
-import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app'; // Corrigido de 'App' para 'AppComponent'
 
-describe('App', () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      imports: [AppComponent], // Corrigido aqui também
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
+  it(`should have the 'Ateliê Luz da Alma' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    // Certifique-se que o título aqui bate com o que está no app.ts
+    expect(app.title).toEqual('Ateliê Luz da Alma');
+  });
+
   it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, atelie-mandalas');
+    // Verifica se existe um h1 (que está na sua Hero Section)
+    expect(compiled.querySelector('h1')?.textContent).toContain('Harmonia em cada traço');
   });
 });
